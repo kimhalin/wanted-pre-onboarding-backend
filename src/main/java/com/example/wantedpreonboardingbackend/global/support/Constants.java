f936944d77a04bc5ac8ecc74f0dee9f0385200ec
HaLin Kim <halin130@tukorea.ac.kr>
2023-08-03 10:58:50 +0900

    chore: constant 추가

---
+++ src/main/java/com/example/wantedpreonboardingbackend/global/support/Constants.java
@@ -0,0 +1,12 @@
+package com.example.wantedpreonboardingbackend.global.support;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+    // Members
+    public static final int MIN_PASSWORD_LENGTH = 8;
+    public static final String EMAIL_REGEX = ".*@.*";
+}

0add2c59516697775639838109388520f0bc79f1
HaLin Kim <halin130@tukorea.ac.kr>
2023-08-04 19:53:27 +0900

    feat: 로그인 인터셉터 추가

---
+++ src/main/java/com/example/wantedpreonboardingbackend/global/support/AuthenticationInterceptor.java
@@ -0,0 +1,30 @@
+package com.example.wantedpreonboardingbackend.global.support;
+
+import com.example.wantedpreonboardingbackend.auth.support.AuthTokenCreator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final AuthTokenCreator authTokenCreator;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        boolean check = checkAnnotation(handler, NoAuth.class);
+        if (!check) {
+            String token = request.getHeader("X-ACCESS-TOKEN");
+            Long userId = this.authTokenCreator.extractPayload(token);
+            request.setAttribute("userId", userId);
+        }
+        return true;
+    }
+
+    private boolean checkAnnotation(Object handler, Class<NoAuth> cls) {
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        return handlerMethod.getMethodAnnotation(cls) != null;
+    }
+}

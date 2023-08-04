69355078b16c862fc3733e646a11b9fa488783eb
HaLin Kim <halin130@tukorea.ac.kr>
2023-08-04 14:19:53 +0900

    fix: response dto에 getter 추가

--- src/main/java/com/example/wantedpreonboardingbackend/member/dto/response/MemberLoginResponse.java
+++ src/main/java/com/example/wantedpreonboardingbackend/member/dto/response/MemberLoginResponse.java
@@ -2,7 +2,9 @@ package com.example.wantedpreonboardingbackend.member.dto.response;
 
 import com.example.wantedpreonboardingbackend.auth.domain.AuthToken;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class MemberLoginResponse {
     private AuthToken authToken;

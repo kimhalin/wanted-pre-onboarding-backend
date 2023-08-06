96cab15d826291ab8527c857e44397e7e9b807f9
HaLin Kim <halin130@tukorea.ac.kr>
2023-08-07 04:13:45 +0900

    feat: 게시글 목록 조회 api 추가

--- src/main/java/com/example/wantedpreonboardingbackend/board/application/BoardService.java
+++ src/main/java/com/example/wantedpreonboardingbackend/board/application/BoardService.java
@@ -4,10 +4,17 @@ import com.example.wantedpreonboardingbackend.auth.domain.AuthInfo;
 import com.example.wantedpreonboardingbackend.board.domain.Board;
 import com.example.wantedpreonboardingbackend.board.domain.BoardRepository;
 import com.example.wantedpreonboardingbackend.board.dto.request.BoardCreateRequest;
+import com.example.wantedpreonboardingbackend.board.dto.response.BoardResponse;
+import com.example.wantedpreonboardingbackend.global.common.PaginatedRequest;
+import com.example.wantedpreonboardingbackend.global.common.PaginatedResponse;
 import com.example.wantedpreonboardingbackend.member.application.MemberService;
 import com.example.wantedpreonboardingbackend.member.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +23,21 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final MemberService memberService;
 
-    public void createBoard(BoardCreateRequest dto, AuthInfo authInfo) {
+    public void createBoard(final BoardCreateRequest dto, final AuthInfo authInfo) {
         Member member = this.memberService.getById(authInfo.getId());
 
         Board board = dto.toEntity(member);
         this.boardRepository.save(board);
+    }
+
+    public PaginatedResponse<BoardResponse> getAllBoards(final PaginatedRequest dto) {
+        Pageable pageable = dto.toPageRequest();
+        Page<Board> boards = this.boardRepository.findAll(pageable);
+        List<BoardResponse> boardResponseList = boards.getContent().stream()
+                .map(BoardResponse::of).toList();
+
+        return new PaginatedResponse<>(boardResponseList, boards.getTotalPages(), boards.getTotalElements(), boards.getPageable().getPageSize(),
+                boards.getPageable().getPageNumber(), boards.getPageable().getOffset());
+
     }
 }
--- src/main/java/com/example/wantedpreonboardingbackend/board/presentation/BoardController.java
+++ src/main/java/com/example/wantedpreonboardingbackend/board/presentation/BoardController.java
@@ -3,7 +3,11 @@ package com.example.wantedpreonboardingbackend.board.presentation;
 import com.example.wantedpreonboardingbackend.auth.domain.AuthInfo;
 import com.example.wantedpreonboardingbackend.board.application.BoardService;
 import com.example.wantedpreonboardingbackend.board.dto.request.BoardCreateRequest;
+import com.example.wantedpreonboardingbackend.board.dto.response.BoardResponse;
+import com.example.wantedpreonboardingbackend.global.common.PaginatedRequest;
+import com.example.wantedpreonboardingbackend.global.common.PaginatedResponse;
 import com.example.wantedpreonboardingbackend.global.support.annotation.CurrentUser;
+import com.example.wantedpreonboardingbackend.global.support.annotation.NoAuth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -11,10 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/boards")
@@ -30,5 +31,13 @@ public class BoardController {
                                             @CurrentUser @Parameter(hidden = true) AuthInfo authInfo) {
         this.boardService.createBoard(dto, authInfo);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @NoAuth
+    @GetMapping
+    @Operation(summary = "게시글 목록 조회")
+    public ResponseEntity<PaginatedResponse<BoardResponse>> getAllBoards(PaginatedRequest dto) {
+        PaginatedResponse<BoardResponse> result = this.boardService.getAllBoards(dto);
+        return ResponseEntity.ok(result);
     }
 }
